70ae1e841293df20543e813b03f5f81fe71fd45e
Fel-ly <kiokofpm@gmail.com>
2024-03-29 00:51:55 +0300

    db connection

--- src/Expense_Income_Tracker.java
+++ src/Expense_Income_Tracker.java
@@ -1,12 +1,8 @@
 import java.sql.*;
-import java.util.Date;
 
 public class Expense_Income_Tracker {
 
-    public Record record;
-
-    public Expense_Income_Tracker() throws SQLException {
-
+    public Expense_Income_Tracker() {
         String url = "jdbc:mysql://localhost:3306/Expense_Income_Tracker";
         String username = "root";
         String password = "";
@@ -15,43 +11,17 @@ public class Expense_Income_Tracker {
             Connection connection = DriverManager.getConnection(url, username, password);
             Statement statement = connection.createStatement();
 
-            // SQL statement to insert data into the Tracker table
-            String insertQuery = "INSERT INTO tracker (date, description, amount, type) VALUES (?, ?, ?, ?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            // the data insertion is handled in the ExpensesIncomesTracker class
 
-            String date = null;
-            String description = null;
-            double amount = 0;
-            boolean type = false;
-
-            preparedStatement.setString(1, date);
-            preparedStatement.setString(2, description);
-            preparedStatement.setDouble(3, amount);
-            preparedStatement.setBoolean(4, type);
-
-            // execution of the insert statement
-            int rowsAffected = preparedStatement.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Data successfully inserted =) ");
-            } else {
-                System.out.println("Insertion failed =( ");
-            }
-
-            connection.commit();
             statement.close();
             connection.close();
 
-        } catch (SQLException e ){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-        public static void main(String[] args){
-
-            new ExpensesIncomesTracker();
-
-
-        }
-
-
+    public static void main(String[] args) {
+        new ExpensesIncomesTracker();
+    }
 }
